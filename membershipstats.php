b23930a1df7f725ff2950c18873c82d8e973f330
Jo Franz <franz@systopia.de>
2023-04-13 16:31:02 +0200

    civix upgrade 23.02.1

--- membershipstats.php
+++ membershipstats.php
@@ -40,7 +40,6 @@ function membershipstats_civicrm_post($op, $objectName, $objectId, &$objectRef) 
   }
 }
 
-
 /**
  * Implements hook_civicrm_config().
  *
@@ -48,17 +47,6 @@ function membershipstats_civicrm_post($op, $objectName, $objectId, &$objectRef) 
  */
 function membershipstats_civicrm_config(&$config) {
   _membershipstats_civix_civicrm_config($config);
-}
-
-/**
- * Implements hook_civicrm_xmlMenu().
- *
- * @param array $files
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
- */
-function membershipstats_civicrm_xmlMenu(&$files) {
-  _membershipstats_civix_civicrm_xmlMenu($files);
 }
 
 /**
@@ -72,15 +60,6 @@ function membershipstats_civicrm_install() {
   membershipstats_create_customgroup();
 
   _membershipstats_civix_civicrm_install();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
- */
-function membershipstats_civicrm_uninstall() {
-  _membershipstats_civix_civicrm_uninstall();
 }
 
 /**
@@ -98,79 +77,3 @@ function membershipstats_civicrm_enable() {
 
   _membershipstats_civix_civicrm_enable();
 }
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
- */
-function membershipstats_civicrm_disable() {
-  _membershipstats_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @param $op string, the type of operation being performed; 'check' or 'enqueue'
- * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
- *
- * @return mixed
- *   Based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
- *                for 'enqueue', returns void
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
- */
-function membershipstats_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _membershipstats_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
- */
-function membershipstats_civicrm_managed(&$entities) {
-  _membershipstats_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * @param array $caseTypes
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function membershipstats_civicrm_caseTypes(&$caseTypes) {
-  _membershipstats_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function membershipstats_civicrm_angularModules(&$angularModules) {
-_membershipstats_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
- */
-function membershipstats_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _membershipstats_civix_civicrm_alterSettingsFolders($metaDataFolders);
-}
-
